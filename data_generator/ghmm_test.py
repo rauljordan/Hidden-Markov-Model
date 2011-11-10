21a93322225666473782ae2b3cbb10e8c589c5f8
Pontus <gozmo1337@gmail.com>
2011-11-10 18:56:50 +0100

    fixed the seg fault, cause due to the precision in python or convertion in ghmm

--- data_generator/ghmm_test.py
+++ data_generator/ghmm_test.py
@@ -18,34 +18,36 @@ def state_to_string( state_num ):
 
 #system_states = ['straight', 'left_turn', 'right_turn']
 def create_hmm():
-    label_domain = ['straight', 'left_turn', 'right_turn']
     emit_states = range( len(gen_alphabet()) )
     emit_domain = Alphabet( emit_states )
 
     trans_p = [ [0.9, 0.05 , 0.05],
-                [ 0.3, 0,69 ,0.01],
-                [ 0.3, 0.01, 0.69]]
+                [ 0.1, 0,9 ,0.0],
+                [ 0.1, 0.0, 0.9]]
 
     le = len(emit_states)
     emit_p = [ [1.0/le]*le,
                [1.0/le]*le,
                [1.0/le]*le]
 
-    pi = [ 0.95, 0.025, 0.025 ]
+    pi = [ 1, 0, 0 ]
     return HMMFromMatrices( emit_domain,
                             DiscreteDistribution(emit_domain),
                             trans_p,
                             emit_p,
-                            pi,
-                            labelDomain=label_domain
+                            pi
                             ),emit_domain
+################################3######################################
 
 hmm, emit_domain = create_hmm()
+hmm.normalize()
 
-training_set = map(alphabet_to_hmm, vq.convert_dataset(gen.left_turn() ))
+training_set = map(alphabet_to_hmm, vq.convert_dataset(gen.left_turn()) )
 ghmm_training_set = EmissionSequence( emit_domain, training_set)
 
-print emit_domain, training_set
-
+#seq = hmm.sampleSingle(100)
+#print seq
+#hmm.baumWelch( seq )
 hmm.baumWelch( ghmm_training_set )
+
 print hmm
