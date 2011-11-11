1153be3e06f5dd43270ad604f4097c7ac9d8e5ad
Pontus <gozmo@sverige.nu>
2011-11-11 11:22:45 +0100

    training works when its working on the sampled data.

--- data_generator/ghmm_test.py
+++ data_generator/ghmm_test.py
@@ -15,7 +15,7 @@ def hmm_to_alphabet( idx ):
     return gen_alphabet()[idx]
 
 def state_to_string( state_num ):
-    return ['left_turn', 'right_turn'][state_num]
+    return ['straight','left_turn', 'right_turn'][state_num]
 
 def gen_data_set():
     fs = [ gen.left_turn, gen.right_turn, gen.straight ]
@@ -31,9 +31,9 @@ def create_hmm():
                 [ 0.1, 0.1, 0.8]]
 
     le = len(emit_states)
-    emit_p = [ [1.0/le]*le,
-               [1.0/le]*le,
-               [1.0/le]*le]
+    emit_p = [ [0.05, 0, 0, 0, 0, 0, 0, 0.05, 0, 0, 0, 0.9, 0, 0, 0,0,0,0,0,0,0],
+               [0.45, 0, 0, 0, 0, 0, 0, 0.45, 0, 0, 0, 0.1, 0, 0, 0,0,0,0,0,0,0],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.1, 0, 0.45, 0,0,0,0,0.45,0,0]]
 
     pi = [ 1, 0, 0 ]
     return HMMFromMatrices( emit_domain,
@@ -42,13 +42,29 @@ def create_hmm():
                             emit_p,
                             pi
                             ),emit_domain
+
 ######################################################################
 
 hmm, emit_domain = create_hmm()
 print hmm
 hmm.normalize()
+
+##### Training the hmm #######
 for x in gen_data_set(): 
     training_set = map(alphabet_to_hmm, vq.convert_dataset( x ) )
     ghmm_training_set = EmissionSequence( emit_domain, training_set)
-    hmm.baumWelch( ghmm_training_set, nrSteps=1 )
+    ghmm_training_set = hmm.sample(10,50)
+    hmm.baumWelch( ghmm_training_set, nrSteps=1, loglikelihoodCutoff = 0.1 )
     print hmm
+
+##### Classifying #####
+left_turn = gen.left_turn()
+for time_t in xrange(1, len(left_turn)):
+    lower = time_t - 100 if time_t - 100 > 0 else 0
+
+    obs_interval =  map( alphabet_to_hmm, vq.convert_dataset(left_turn[lower:time_t] ) )
+    obs_sequence = EmissionSequence( emit_domain, obs_interval ) 
+   # print obs_interval 
+    state = hmm.viterbi( obs_sequence )
+    print state_to_string( state[0][-1]) , left_turn[time_t]
+        #print state_to_string( state ), left_turn[time_t]
